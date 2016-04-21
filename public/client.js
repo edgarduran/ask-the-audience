770f2e25941204cc3aa66886bff153bbebd76fb6
Edgar Duran <edgar.eduran@gmail.com>
2016-04-21 10:42:12 -0600

    push up almost completed tutorial

---
+++ public/client.js
@@ -0,0 +1,28 @@
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+var messageText = document.getElementById('message');
+var buttons = document.querySelectorAll('#voting-choices button');
+var voteTally = document.getElementById('vote-totals')
+// buttons.forEach(function (button) {
+//   console.log(this.innerText);
+// });
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected users: ' + count;
+});
+
+socket.on('statusMessage', function (message) {
+  messageText.innerText = message;
+});
+
+socket.on('voteCount', function (votes) {
+  console.log(votes);
+  voteTally.innerText = votes
+});
---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+var server = http.createServer(app);
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+var votes = {};
+
+function countVotes (votes) {
+  var voteCount = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+  };
+  for (var vote in votes) {
+    voteCount[votes[vote]]++
+  }
+  return voteCount;
+}
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var port = process.env.PORT || 3000;
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+  io.sockets.emit('usersConnected', io.engine.clientsCount );
+  socket.emit('statusMessage', 'You are now connected');
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    console.log(votes);
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('usersConnected', io.engine.clientsCount );
+  });
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+      socket.emit('voteCount', countVotes(votes));
+    }
+  });
+});
+
+server.listen(port, function () {
+  console.log('Listening on port ' + port + '.');
+});
+
+module.exports = server;
